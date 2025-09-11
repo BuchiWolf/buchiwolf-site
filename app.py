390c8b4f0ca5b28cfe09f33a3d221dc2ca63aaaa
BuchiWolf <903271561@qq.com>
2025-09-11 16:45:02 +0800

    test

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>你好，BuchiWolf 的网站上线了！</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
